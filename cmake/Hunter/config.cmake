110e0c9892b55730ba5b70d916f019bd41087a3b
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-05-05 23:10:15 -0400

    added SPIRV-Headers

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,6 @@
+hunter_config(SPIRV-Headers
+    VERSION v1.3.7-p0
+    URL https://github.com/mchiasson/SPIRV-Headers/archive/v1.3.7-p0.tar.gz
+    SHA1 a1f5ca33c3492b8ca87dcea1e8efedc5073967df
+    CMAKE_ARGS SPIRV_HEADERS_ENABLE_EXAMPLES=OFF
+)
